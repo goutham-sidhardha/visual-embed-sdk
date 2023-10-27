0d760b51ce2971b8517a8448aeb2d1a7c6b92279
sastaachar <justinjohnmathew100@gmail.com>
2023-10-27 18:30:38 +0530

    optional loglevel

--- src/types.ts
+++ src/types.ts
@@ -535,7 +535,7 @@ export interface EmbedConfig {
     /**
      * Loglevel for the SDK
      */
-    logLevel: LogLevel;
+    logLevel?: LogLevel;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
